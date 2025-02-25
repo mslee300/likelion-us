220dfe9beb482d0b5636e43342482dd9fcd3550c
Minseok Benjamin Lee <55467050+mslee300@users.noreply.github.com>
2025-02-25 14:33:41 +0000

    backup 4

--- likelion/views.py
+++ likelion/views.py
@@ -27,6 +27,7 @@ def search(request):
     tweets = Tweet.objects.filter(
         Q(content__icontains=query)
         | Q(author__username__icontains=query)) if query else []
+    ai_analysiss = AIAnalysis.objects.filter(tweet__in=tweets)
     people = Author.objects.filter(
         Q(name__icontains=query)
         | Q(username__icontains=query)) if query else []
@@ -49,36 +50,59 @@ def search(request):
             'companies': companies,
             'people': people,
             'query': query,
+            'ai_analysiss': ai_analysiss
         })
 
 
 def ai_analysis(request):
-    return render(request, 'likelion/ai-analysis.html')
+    id = request.GET.get('id')
+    ai_analysis = AIAnalysis.objects.get(id=id)
+
+    # print(ai_analysis.tweet.content)
+    return render(request, 'likelion/ai-analysis.html',
+                  {'analysis': ai_analysis})
 
 
 def person(request):
     person_id = request.GET.get('q', '')
+    tweets = Tweet.objects.filter(author__id=person_id)
+    ai_analysiss = AIAnalysis.objects.filter(tweet__in=tweets)
     person = Author.objects.get(id=person_id)
-    # {
-    #     name: "Elon Musk",
-    #     username: "elonmusk",
-    #     profile_image_url: "https://pbs.twimg.com/profile_images/1542248939/elon-musk-twitter"
-    #    'description': "description",
-    #    'companies': ['tesla', 'spacex', 'twitter']
-    # }
 
     return render(request, 'likelion/person.html', {
         'person': person,
+        'ai_analysiss': ai_analysiss
     })
 
+def company(request):
+    query = request.GET.get('q', '')
 
-def company(request, company_id):
-    company_id = request.GET.get('q', '')
-    company = Company.objects.filter(id=int(company_id))
-    tweets = Tweet.objects.filter(companies=company)
+    tweets = Tweet.objects.filter(
+        Q(content__icontains=query)
+        | Q(author__username__icontains=query)) if query else []
+    ai_analysiss = AIAnalysis.objects.filter(tweet__in=tweets)
+    people = Author.objects.filter(
+        Q(name__icontains=query)
+        | Q(username__icontains=query)) if query else []
+    companies = Company.objects.filter(
+        Q(name__icontains=query) | Q(ticker__icontains=query)
+        | Q(description__icontains=query)) if query else []
 
-    context = {
+    # 로고 URL을 가져오기 위한 API 호출
+    for company in companies:
+        ticker = company.ticker
+        api_url = f'https://api.twelvedata.com/logo?symbol={ticker}&apikey=c79378a0eb5b414fb3b8b54b84e64a06'
+        response = requests.get(api_url)
+        data = response.json()
+
+        # 로고 URL을 company 객체에 저장 (또는 템플릿에서 바로 사용)
+        company.logo_url = data.get('url', '')
+    return render(
+        request, 'likelion/company.html', {
             'tweets': tweets,
-            'companies': company,
-    }
+            'companies': companies,
+            'people': people,
+            'query': query,
+            'ai_analysiss': ai_analysiss
+        })
     return render(request, 'likelion/company.html', context)
