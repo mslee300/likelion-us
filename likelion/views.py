e03430ba4516f90fab22d8f5fe3ab22ac14db395
Minseok Benjamin Lee <55467050+mslee300@users.noreply.github.com>
2025-02-25 13:25:32 +0000

    backup 3

--- likelion/views.py
+++ likelion/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Author, Company, Tweet, AIAnalysis, Vote, Comment, UserProfile, Notification
 from django.db.models import Q
+import requests
 
 
 def index(request):
@@ -33,6 +34,15 @@ def search(request):
         Q(name__icontains=query) | Q(ticker__icontains=query)
         | Q(description__icontains=query)) if query else []
 
+    # 로고 URL을 가져오기 위한 API 호출
+    for company in companies:
+        ticker = company.ticker
+        api_url = f'https://api.twelvedata.com/logo?symbol={ticker}&apikey=c79378a0eb5b414fb3b8b54b84e64a06'
+        response = requests.get(api_url)
+        data = response.json()
+
+        # 로고 URL을 company 객체에 저장 (또는 템플릿에서 바로 사용)
+        company.logo_url = data.get('url', '')
     return render(
         request, 'likelion/search.html', {
             'tweets': tweets,
@@ -62,7 +72,13 @@ def person(request):
     })
 
 
-def company(request):
-    return render(request, 'likelion/company.html', {
-        'company': "",
-    })
+def company(request, company_id):
+    company_id = request.GET.get('q', '')
+    company = Company.objects.filter(id=int(company_id))
+    tweets = Tweet.objects.filter(companies=company)
+
+    context = {
+            'tweets': tweets,
+            'companies': company,
+    }
+    return render(request, 'likelion/company.html', context)
